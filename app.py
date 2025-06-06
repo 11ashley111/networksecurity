8efc9978cead8df1b9f736957d956592bb69db91
“ASHLEY” <ashlyyy6969@gmail.com>
2025-06-07 00:45:02 +0530

    update

--- app.py
+++ app.py
@@ -111,5 +111,5 @@ async def predict_url(request: Request, url: str = Form(...)):
         })
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT",8000))
+    port = int(os.environ.get("PORT",10000))
     uvicorn.run("app:app", host="0.0.0.0", port=port)
